4c69e255d5f69cde967e41571a84b5afc6084fa4
Daniel Robertson <robertsondaniel542@gmail.com>
2019-09-23 11:29:27 +1000

    first commit

---
+++ scripts/forcast.js
@@ -0,0 +1,9 @@
+const key = '9kmxTw106hOwBksaoMGrAJGtAQ531Xs6';
+
+const getCity = async (city) =>{
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+    const query = '?apiKey=${key}&q=${city}';
+
+};
