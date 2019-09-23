778961249aae8e4c0c32fe1232d6da6b9864a0ce
Kats <katsu.0323.jpn@icloud.com>
2019-09-23 11:47:54 +1000

    Get city Api call

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,9 +1,17 @@
 const key = '9kmxTw106hOwBksaoMGrAJGtAQ531Xs6';
 
-const getCity = async (city) =>{
+const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
 
-    const query = '?apiKey=${key}&q=${city}';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data =  await response.json();
+
+return (data[0]);
 
 };
+getCity("sydney")
+.then(data => console.log(data))
+.catch(err => console.log(err));
