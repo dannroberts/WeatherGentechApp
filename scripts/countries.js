3ea11c5c385ffcc0c768faeb1cc1056a88ad92ac
Kats <katsu.0323.jpn@icloud.com>
2019-09-24 13:56:03 +1000

    change design

---
+++ scripts/countries.js
@@ -0,0 +1,16 @@
+// fetch ('https://restcountries.eu/rest/v2/all')
+// .then(response => {
+//     return response.json();
+// }) .then(data => {
+//     const data1 = data[0]
+//     console.log(data1.flag);
+
+//     document.body.style.backgroundImage = data1.flag;
+// });
+
+
+
+
+   
+            // getPicture();
+            module.exports = getPicture;
--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,17 +1,16 @@
-const key = 'wEyczSYnpjqplmMnTVVIdFUTy64D5q8p';
+const key = '9kmxTw106hOwBksaoMGrAJGtAQ531Xs6';
 
 const getWeather = async (id) => {
 
-const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-const query = `${id}?apikey=${key}`;
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
     const data =  await response.json();
 
     return data[0];
-
-
 };
+
 
 const getCity = async (city) => {
 
@@ -19,10 +18,34 @@ const getCity = async (city) => {
 
     const query = `?apikey=${key}&q=${city}`;
 
+    // get country from the data retreived
+    
+    
     const response = await fetch(base + query);
     const data =  await response.json();
+        
+   
+
+    var countryName = data[0].Country.EnglishName;
+    // console.log(countryName);
+    getPicture(countryName);
 
 return (data[0]);
 
 };
 
+
+function getPicture(countryName){
+    fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+        .then(response => response.json())
+        .then(data =>{
+            
+            let result = data[0];
+            console.log(result);
+            document.body.style.backgroundImage = `url(${result.flag})`;
+            document.body.style.backgroundRepeat = "no-repeat";
+            document.body.style.backgroundSize = "cover";
+            
+        })
+    .catch(err => console.log(err))};
+    
