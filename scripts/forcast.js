ed972b67d75d44291af7c261e136e52adfe72bbe
Daniel Robertson <robertsondaniel542@gmail.com>
2019-09-24 10:38:23 +1000

    uu

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -16,12 +16,10 @@ const query = `${id}?apikey=${key}`;
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-
     const query = `?apikey=${key}&q=${city}`;
-
     const response = await fetch(base + query);
     const data =  await response.json();
 
-return (data[0]);
+    return (data[0]);
 
 };
